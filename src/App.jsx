fe6bf7c81f77d8b498090370d3ae1c44bd14ea1d
Nhoj Amosin <amosinnhoj@gmail.com>
2024-07-18 22:06:52 +0800

    choices shuffled

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,17 @@ function App() {
     return hold;
   }
 
-  quizObject.choices = choices(quizObject.correct_answer, decoded);
+  const unshuffled = choices(quizObject.correct_answer, decoded);
+
+  const shuffle = (arr) => {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  };
+
+  quizObject.choices = unshuffled.map((shuffled) => shuffle(shuffled));
 
   return (
     <BrowserRouter>
